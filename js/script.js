8f0706a3c4159c364f4efa5af169c177dfb69b43
amawre <amanda.wrede@chasacademy.se>
2021-12-16 13:57:58 +0100

    makes player enabled to keep playing after win/loose

--- js/script.js
+++ js/script.js
@@ -16,32 +16,33 @@ const message = document.querySelector(".message");
 let rightGuesses = 0;
 let wrongGuesses = 0;
 
+
 // ===== Function that takes a random word from "words" and then split that word in to an array =====
 function randomWord() {
     const randomWord = words[Math.floor(Math.random() * words.length)];
     wordArray = randomWord.split("");
 }
 // ===== Adds eventListener to startBt. Waits for click on the startBtn then it calls both createPH and randomword =====
-startBtn.addEventListener("click", createPH);
-console.log(startBtn);
+startBtn.addEventListener("click", placeholders);
 randomWord();
 
 /* ===== Function that creates place holders for the current word to be guessed. For each letter in the word it 
          creates an empty li and place them in the ul in HTML that is marked with the class currentWord. If the 
          "word" includes a space it takes away the styling for it.
-         Then removes eventlistener to prevent multiple word at the same time */
-function createPH() {
+         Then removes eventlistener to prevent multiple word at the same time. Makes buttons visible*/
+function placeholders() {
     wordArray.forEach(item => {
         const li = document.createElement("li");
         if(item === " ") {
             li.style.borderBottom = "none";
         }
         currentWord.appendChild(li);
-        startBtn.removeEventListener("click", createPH);
-    })    
+        startBtn.removeEventListener("click", placeholders);
+        letterBtn.style.visibility = "visible";
+    })      
 }
 
-// ===== Adds eventlistener to letterBtn. When clicked on it calls the function compmare =====
+// ===== Adds eventlistener to letterBtn. When clicked on it calls the function compare =====
 letterBtn.addEventListener("click", compare);
 
 /* ===== Function wich first checks if clicked element is a button. Then it listens to the click if you click a button
@@ -58,14 +59,16 @@ function compare(e) {
                              so you can win the game without filling every place holder =====*/
                     if(wordArray.includes(" ")) {
                         if(rightGuesses === wordArray.length -1) {
-                            message.innerHTML = "The force is strong with you!"
+                            message.innerHTML = "The force is strong with you!";
+                            letterBtn.style.visibility = "hidden"; 
                             startOverBtn();
                         } 
                     /* ===== When the rightGuesses is equal to the length of the word you win and get a message. And 
                               the startOverBtn function is called =====*/
                     } else if(!wordArray.includes(" ")) {
                         if(rightGuesses === wordArray.length) {
-                            message.innerHTML = "The force is strong with you!"
+                            message.innerHTML = "The force is strong with you!";
+                            letterBtn.style.visibility = "hidden"; 
                             startOverBtn();
                         }
                     }
@@ -82,28 +85,31 @@ function compare(e) {
                 message.innerHTML = "One more wrong and you're done...";
             }
 
-            if(wrongGuesses === 6) {
+            if(wrongGuesses === 6) {                
                 message.innerHTML = "Jabba the Hutt will be so pleased with his new pet, YOU!";
-                startOverBtn();
+                letterBtn.style.visibility = "hidden";              
+                startOverBtn();                
             }
+
         }
+    }
         // ===== Disables buttons after beeing clicked =====
         e.target.disabled = true;
-    }
 }
+
 
 // ===== Function that changes the text in the startBtn and adds an eventlistener and calls function starOverF =====
 function startOverBtn() {
     startBtn.textContent = "Start over";
-    startBtn.addEventListener("click", startOverF);
+    startBtn.addEventListener("click", startOver);
 }
 
 /* ===== Function that resets the game. Change the button text back to "Start Game", empties the word container, 
          resets the counters and image, enables buttons ===== */
-function startOverF() {
+function startOver() {
     startBtn.textContent = "Start Game";
     randomWord();
-    startBtn.addEventListener("click", createPH);
+    startBtn.addEventListener("click", placeholders);
     currentWord.innerHTML = "";
     rightGuesses = 0;
     wrongGuesses = 0;
@@ -112,5 +118,5 @@ function startOverF() {
     const enableBtns = document.getElementsByTagName("button");
         for (let i = 0; i < enableBtns.length; i++) {
             enableBtns[i].disabled = false;
-        }
+        } 
 }
